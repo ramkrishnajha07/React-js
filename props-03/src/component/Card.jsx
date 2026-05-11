615cfb8a9b20b087f7d126b7da7d687aca0c9a15
ram krishna jha <124547998+ramkrishnajha07@users.noreply.github.com>
2026-05-11 11:01:46 +0530

    Save current React project changes

---
+++ props-03/src/component/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="cards">
+      <img
+        src="https://images.unsplash.com/photo-1771132666487-3d7a048a36df?q=80&w=978&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+
+      <h1>ram krishna jha</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <button>view profile</button>
+    </div>
+  )
+}
+
+export default Card
